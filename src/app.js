05892845c029f9b138d98bbe8d25ad311783b483
Tarcísio Teles <tfst@ecomp.poli.br>
2021-10-23 13:58:46 -0300

    feat: user sign-up/sign-in wwith bcrypt

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 
-import { listUsers, createUser } from './controllers/users.js';
+import { listUsers, createUser, userLogIn } from './controllers/users.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/users', listUsers);
-app.post('/users', createUser);
+app.post('/create-user', createUser);
+
+app.post('/sign-in', userLogIn);
 
 
 
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,5 @@
 import connection from "../database/database.js";
+import bcrypt from "bcrypt";
 
 async function listUsers (req, res) {
     try {
@@ -11,32 +12,51 @@ async function listUsers (req, res) {
         return res.status(200).send(result.rows);
     }
     catch (error) {
-        return res.status(501).send(error);
+        return res.status(500).send(error);
     }
 }
 
 async function createUser (req, res) {
     const {name, cpf, birthDate, email, password} = req.body;
+    const encripted = bcrypt.hashSync(password, 10);
     try {
         const result = await connection.query(`
         INSERT INTO users (name, cpf, "birthDate", email, password) VALUES ($1, $2, $3, $4, $5);
-        `, [name, cpf, birthDate, email, password]);
+        `, [name, cpf, birthDate, email, encripted]);
         
         return res.status(200).send("novo usuario cadastrado com sucesso");
-
     }
     catch (error) {
-        return res.sendStatus(501);
-
+        console.log(error);
+        return res.status(409).send(error.code + ": " + error.detail);
     }
+}
 
+async function userLogIn (req, res) {
+    const {email, password} = req.body;
+    try {
+        const userData = await connection.query(`
+            SELECT * FROM users WHERE email = $1;
+        `, [email]);
+
+        if (userData.rowCount === 0) {
+            return res.status(401).send("email e/ou senha incorretos");
+        }
+                
+        const user = userData.rows[0];
+        if (!(user && bcrypt.compareSync(password, user.password))) {
+            return res.status(401).send("email e/ou senha incorretos");
+        }
+
+        return res.status(200).send(`usuário logado: ${user.name} ${user.email}`);        
+    }
+    catch (error) {
+        console.log(error);
+        return res.sendStatus(501);
+    }
 }
 
 
 
 
-
-
-
-
-export { listUsers, createUser };
+export { listUsers, createUser, userLogIn };
